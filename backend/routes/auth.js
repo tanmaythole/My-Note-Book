d318d8f0ee4193b89e02609b21e3a2e6e4c1d511
tanmaythole <tanmaythole@gmail.com>
2021-09-03 22:28:31 +0530

    Update

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -8,26 +8,30 @@ router.get('/', (req, res)=>{
 })
 
 router.post('/signup',[
-    body('email', "Enter Valid Email").isEmail(),
-    body('password', "Password Mush be more than 4 characters").isLength({min:4}),
-    body('name', "Name Should be more than 2 characters").isLength({min:2}),
-], (req, res)=>{
-    // If error occured
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
+        body('email', "Enter Valid Email").isEmail(),
+        body('password', "Password Mush be more than 4 characters").isLength({min:4}),
+        body('name', "Name Should be more than 2 characters").isLength({min:2}),
+    ], async (req, res)=>{
+        // If error occured
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+        }
 
-    User.create({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password,
+        let user = await User.findOne({email: req.body.email});
+        if(user){
+            return res.status(400).json({error: "Email Already Taken"})
+        }
+        User.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password,
+        })
+        .then(user => res.json(user))
+        .catch(err => {
+            res.json({error:err, message:err.message})
+        });
     })
-    .then(user => res.json(user))
-    .catch(err => {
-        res.json({error:err, message:err.message})
-    });
-})
 
 router.post('/login', (req, res)=>{
     res.send("Login");
