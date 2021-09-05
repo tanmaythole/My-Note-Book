c96ece74d09f049e2e968f10cff75964e400852b
tanmaythole <tanmaythole@gmail.com>
2021-09-05 17:33:28 +0530

    created auth token,store hash password

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,6 +2,10 @@ const express = require('express');
 const User = require('../models/User');
 const router = express.Router();
 const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const JWT_SECRET_KEY = 'My-note-book-tanmay-764464';
 
 router.get('/', (req, res)=>{
     res.send("hello");
@@ -22,15 +26,28 @@ router.post('/signup',[
         if(user){
             return res.status(400).json({error: "Email Already Taken"})
         }
-        User.create({
+
+        // creating secure hash password using bcrytjs
+        const salt = await bcrypt.genSalt(10);
+        const secPass = await bcrypt.hash(req.body.password, salt);
+
+        user = User.create({
             name: req.body.name,
             email: req.body.email,
-            password: req.body.password,
-        })
-        .then(user => res.json(user))
-        .catch(err => {
-            res.json({error:err, message:err.message})
+            password: secPass,
         });
+
+
+        // creating auth token using jsonwebtoken
+        const data = {
+            user:{
+                id: user.id
+            }
+        }
+
+        const authToken = jwt.sign(data, JWT_SECRET_KEY);
+
+        res.send({authToken});
     })
 
 router.post('/login', (req, res)=>{
