8ab35b2811582dd92b0e336dba5876c8d972af05
tanmaythole <tanmaythole@gmail.com>
2021-09-06 22:50:23 +0530

    code cleaned up

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,9 @@
 import React, {useContext, useEffect} from 'react'
-import NoteContext from '../context/notes/NoteContext'
 
 const About = () => {
-    const a = useContext(NoteContext)
-    useEffect(() => {
-        a.update()
-    }, [])
     return (
         <div>
-            <h1>About Page - {a.state.name}</h1>
+            <h1>About Page</h1>
         </div>
     )
 }
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -1,22 +1,8 @@
 import NoteContext from "./NoteContext";
-import { useState } from "react";
 
 const NoteState = (props) => {
-    const s1 = {
-        "name": "Tanmay",
-        "age": "21"
-    }
-    const [state, setstate] = useState(s1);
-    const update = () => {
-        setTimeout(()=>{
-            setstate({
-                "name": "Tanmay Thole",
-                "age": "21"
-            })
-        }, 1000);
-    }
     return(
-        <NoteContext.Provider value={{state, update}}>
+        <NoteContext.Provider value={{}}>
             {props.children}
         </NoteContext.Provider>
     )
